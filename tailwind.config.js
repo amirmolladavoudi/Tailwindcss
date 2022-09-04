3fcbf7a0b43f15ede5415ce516dab26e23b5cf34
amirmolladavoudi <amirmolladavodi@gmail.com>
2022-09-05 00:06:22 +0430

    complet the progect
    I'll finish this in one and half days.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      inset: {
+        '176': '44rem',
+      },
+    },
+  },
+  plugins: [],
+}
